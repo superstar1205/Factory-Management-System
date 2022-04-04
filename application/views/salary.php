7301f1a7ace70061b99e0aba517ff05816fd3449
Baymax <mulanohs@gmail.com>
2022-04-04 06:21:03 -0700

    Update salary.php

--- application/views/salary.php
+++ application/views/salary.php
@@ -168,107 +168,103 @@
                                         <!-- end row -->
                                     </div>
                                 </div>
-
                                 <div class="col-12 rest" id="rest" style="display: none;">
-                                <div class="card" >
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col-lg-12">
-                                                <div class="card " id="">
-                                                    <div class="card-body">
-                                                    <br><br><br><br>
-                                                            <h2 class="text-center">Senuli Fashion</h2>
-                                                            <h3 class="text-center">Monthly Salary Information</h3>
-                                                            <h4 class="text-center"><?php echo $employee_name;?></h4>
-                                                            <div class="invoice-title mb-2" style="margin-top: 140px;">
-                                                                <div class="row">
-                                                                    <div class="form-group col-md-6 text-center">
-                                                                        <label for="Total Amount">Month : <?php echo $srch_month;?></label>
-                                                                    </div>
-                                                                    <div class="form-group col-md-6 text-center">
-                                                                        <label for="Total Amount">Date : <?php echo date('Y-m-d');?></label>
+                                    <div class="card" >
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-lg-12">
+                                                    <div class="card " id="">
+                                                        <div class="card-body">
+                                                        <br><br><br><br>
+                                                                <h2 class="text-center">Senuli Fashion</h2>
+                                                                <h3 class="text-center">Monthly Salary Information</h3>
+                                                                <h4 class="text-center"><?php echo $employee_name;?></h4>
+                                                                <div class="invoice-title mb-2" style="margin-top: 140px;">
+                                                                    <div class="row">
+                                                                        <div class="form-group col-md-6 text-center">
+                                                                            <label for="Total Amount">Month : <?php echo $srch_month;?></label>
+                                                                        </div>
+                                                                        <div class="form-group col-md-6 text-center">
+                                                                            <label for="Total Amount">Date : <?php echo date('Y-m-d');?></label>
+                                                                        </div>
                                                                     </div>
                                                                 </div>
-                                                            </div>
-                                                            <div class="table-responsive">
-                                                                <table class="table table-nowrap">
-                                                                    <thead>
-                                                                        <tr>
-                                                                            <th style="width: 15%;" class="text-center">No.</th>
-                                                                            <th style="width: 35%;" class="text-center">Customer Name</th>
-                                                                            <th style="width: 25%;" class="text-center">Completed Pieces</th>
-                                                                            <th style="width: 25%;" class="text-center">Order Payment</th>
-                                                                        </tr>
-                                                                    </thead>
-                                                                    <tbody>
-                                                                        <?php 
-                                                                            if(count($cpop_list) > 0){
-                                                                                foreach($cpop_list as $key => $val){
-                                                                        ?>
+                                                                <div class="table-responsive">
+                                                                    <table class="table table-nowrap">
+                                                                        <thead>
                                                                             <tr>
-                                                                                <td class="text-center">
-                                                                                    <?php echo $key+1;?>
+                                                                                <th style="width: 15%;" class="text-center">No.</th>
+                                                                                <th style="width: 35%;" class="text-center">Customer Name</th>
+                                                                                <th style="width: 25%;" class="text-center">Completed Pieces</th>
+                                                                                <th style="width: 25%;" class="text-center">Order Payment</th>
+                                                                            </tr>
+                                                                        </thead>
+                                                                        <tbody>
+                                                                            <?php 
+                                                                                if(count($cpop_list) > 0){
+                                                                                    foreach($cpop_list as $key => $val){
+                                                                            ?>
+                                                                                <tr>
+                                                                                    <td class="text-center">
+                                                                                        <?php echo $key+1;?>
+                                                                                    </td>
+                                                                                    <td class="text-center">
+                                                                                        <?php echo $val['customer_name'];?>
+                                                                                    </td>
+                                                                                    <td class="text-center">
+                                                                                        <?php echo $val['cp'];?>
+                                                                                    </td>
+                                                                                    <td class="text-center">
+                                                                                        Rs.<?php echo $val['op'];?>
+                                                                                    </td>
+                                                                                </tr>
+                                                                            <?php
+                                                                                    }
+                                                                                }else{
+                                                                            ?>
+                                                                            <tr>
+                                                                                <td colspan="6" class="border-0 text-center">No Data</td>                                                                            
+                                                                            </tr>
+                                                                            <?php
+                                                                                }
+                                                                            ?>
+                                                                            <tr>
+                                                                                <td colspan="5" class="border-0 text-right">Paid Amount</td>
+                                                                                <td class="border-0 text-right">Rs.<?php echo $paid_amount;?></td>
+                                                                            </tr>
+                                                                            <tr>
+                                                                                <td colspan="5" class="border-0 text-right">Unpaid Amount</td>
+                                                                                <td class="border-0 text-right">Rs.<?php echo $unpaid_amount;?></td>
+                                                                            </tr>
+                                                                            <tr>
+                                                                                <td colspan="5" class="border-0 text-right">
+                                                                                    <strong>Total Amount</strong>
                                                                                 </td>
-                                                                                <td class="text-center">
-                                                                                    <?php echo $val['customer_name'];?>
-                                                                                </td>
-                                                                                <td class="text-center">
-                                                                                    <?php echo $val['cp'];?>
-                                                                                </td>
-                                                                                <td class="text-center">
-                                                                                    Rs.<?php echo $val['op'];?>
+                                                                                <td class="border-0 text-right">
+                                                                                    <h4 class="m-0">Rs.<?php echo $total_amount;?></h4>
                                                                                 </td>
                                                                             </tr>
-                                                                        <?php
-                                                                                }
-                                                                            }else{
-                                                                        ?>
-                                                                        <tr>
-                                                                            <td colspan="6" class="border-0 text-center">No Data</td>                                                                            
-                                                                        </tr>
-                                                                        <?php
-                                                                            }
-                                                                        ?>
-                                                                        <tr>
-                                                                            <td colspan="5" class="border-0 text-right">Paid Amount</td>
-                                                                            <td class="border-0 text-right">Rs.<?php echo $paid_amount;?></td>
-                                                                        </tr>
-                                                                        <tr>
-                                                                            <td colspan="5" class="border-0 text-right">Unpaid Amount</td>
-                                                                            <td class="border-0 text-right">Rs.<?php echo $unpaid_amount;?></td>
-                                                                        </tr>
-                                                                        <tr>
-                                                                            <td colspan="5" class="border-0 text-right">
-                                                                                <strong>Total Amount</strong>
-                                                                            </td>
-                                                                            <td class="border-0 text-right">
-                                                                                <h4 class="m-0">Rs.<?php echo $total_amount;?></h4>
-                                                                            </td>
-                                                                        </tr>
-                                                                    </tbody>
-                                                                </table>
+                                                                        </tbody>
+                                                                    </table>
+                                                                </div>
+                                                            
                                                             </div>
-                                                        
-                                                        </div>
-                                                        <div class="d-print-none">
-                                                            <div class="float-right">
-                                                                <a href="javascript:window.print()" class="btn btn-success waves-effect waves-light mr-1" style="width: 100px;" title="Print"><i class="fa fa-print"></i></a>
-                                                                <a href="<?php echo base_url('employee');?>" class="btn btn-primary w-md waves-effect waves-light">Back</a>
+                                                            <div class="d-print-none">
+                                                                <div class="float-right">
+                                                                    <a href="javascript:window.print()" class="btn btn-success waves-effect waves-light mr-1" style="width: 100px;" title="Print"><i class="fa fa-print"></i></a>
+                                                                    <a href="<?php echo base_url('employee');?>" class="btn btn-primary w-md waves-effect waves-light">Back</a>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-                                        <!-- end row -->
                                     </div>
-                                </div>
-                                </div>
-                                                                        
+                                </div>                                      
                             </div>
                         </div>
-                        <!-- end row -->
-                    </div> <!-- container-fluid -->
+                    </div>
                 </div>
 
                 <script>
